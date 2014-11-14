e22f9d5e39338085c62d5050be7201847a09bab0
zhan hua <zhanhua@zhanMac.local>
2014-11-14 15:13:14 +0800

    Initial Commit

---
+++ helloworld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by zhan hua on 14-11-14.
+//  Copyright (c) 2014年 zhan hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ helloworld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  helloworld
+//
+//  Created by zhan hua on 14-11-14.
+//  Copyright (c) 2014年 zhan hua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
